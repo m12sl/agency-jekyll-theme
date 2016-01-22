cf11c00e3ac13447bc64ccb28ca045616dec4a24
Alex Ozerin <m12sl@yandex.ru>
2016-01-22 03:27:10 +0300

    change all to deephack

---
+++ img/team/2_files/PSzoH7FR7MR.js
@@ -0,0 +1,5 @@
+/*!CK:223795743!*//*1450741036,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["botWg"]); }
+
+__d("XDOMScannerResultsController",["XController"],function a(b,c,d,e,f,g){c.__markCompiled&&c.__markCompiled();f.exports=c("XController").create("\/mac_nerd_son\/",{});},null);
---
+++ img/team/2_files/WAmlPPD1l9J.js
@@ -0,0 +1,5 @@
+/*!CK:1238446415!*//*1452757742,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["8eJ2b"]); }
+
+__d('legacy:PhotoSnowliftAds',['PhotoSnowliftAds'],function a(b,c,d,e){if(c.__markCompiled)c.__markCompiled();b.PhotoSnowliftAds=c('PhotoSnowliftAds');},3);
